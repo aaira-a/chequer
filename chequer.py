4d66bc6bad3b58c0a1cbe15b16892e65f8464e48
azam alias <azamalias@gmail.com>
2015-04-03 08:31:30 +0800

    added single digit parser

--- chequer.py
+++ chequer.py
@@ -1,4 +1,9 @@
 
+WORDS = {'1': 'satu', '2': 'dua', '3': 'tiga', '4': 'empat', '5': 'lima',
+         '6': 'enam', '7': 'tujuh', '8': 'lapan', '9': 'sembilan',
+         }
+
+
 def get_digits(numstr):
     return numstr.split('.')[0]
 
@@ -8,3 +13,7 @@ def get_decimals(numstr):
         return numstr.split('.')[1]
     except:
         ValueError
+
+
+def parse_single_digit(numstr):
+    return WORDS[numstr]
--- test_chequer.py
+++ test_chequer.py
@@ -1,6 +1,7 @@
 from chequer import (
     get_decimals,
     get_digits,
+    parse_single_digit,
     )
 
 import unittest
@@ -19,3 +20,15 @@ class GetDigitsGetDecimalsTest(unittest.TestCase):
 
     def test_get_decimals_for_number_with_no_decimals(self):
         self.assertEqual(get_decimals('567'), None)
+
+
+class ParseSingleDigitTest(unittest.TestCase):
+
+    def test_1_to_9(self):
+        expected_results = {
+            '1': 'satu', '2': 'dua', '3': 'tiga', '4': 'empat',
+            '5': 'lima', '6': 'enam', '7': 'tujuh', '8': 'lapan',
+            '9': 'sembilan'}
+
+        for key in expected_results.keys():
+            self.assertEqual(parse_single_digit(key), expected_results[key])
