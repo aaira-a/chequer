d78829ebed2a76d1cf9070cb8f470bd302a2d28d
azam alias <azamalias@gmail.com>
2015-04-12 23:35:04 +0800

    finished rudimentary flask app, with only single view

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
+from chequer import chequer_main
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    input = request.args.get('number')
+    return render_template('index.html', text=chequer_main(input))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
--- test_app.py
+++ test_app.py
@@ -11,3 +11,11 @@ class AppTestCase(unittest.TestCase):
     def test_index_view_should_use_correct_template(self):
         response = self.app.get('/')
         self.assertIn(b'<title>Chequer</title>', response.data)
+
+    def test_index_view_with_querystring_also_uses_correct_template(self):
+        response = self.app.get('/?number=1234567.89')
+        self.assertIn(b'<title>Chequer</title>', response.data)
+
+    def test_index_view_with_querystring_returns_correct_output(self):
+        response = self.app.get('/?number=123.45')
+        self.assertIn(b'RINGGIT MALAYSIA SATU RATUS DUA PULUH TIGA DAN SEN EMPAT PULUH LIMA SAHAJA', response.data)
